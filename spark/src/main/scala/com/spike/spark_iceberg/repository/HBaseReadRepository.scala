ed8381832ca5b14540b95f20eab2fe715f4285ae
anuj-mehra <mehra.anuj1712@gmail.com>
2024-07-12 20:45:37 +0530

    @anuj-mehra code updated for hbase read/write

--- spark/src/main/scala/com/spike/spark_iceberg/repository/HBaseReadRepository.scala
+++ spark/src/main/scala/com/spike/spark_iceberg/repository/HBaseReadRepository.scala
@@ -1,6 +1,15 @@
+// scalastyle:off
 package com.spike.spark_iceberg.repository
 
-class HBaseReadRepository {
+import com.typesafe.scalalogging.LazyLogging
+import org.apache.hadoop.conf.Configuration
+import org.apache.spark.sql.DataFrame
+
+class HBaseReadRepository(@transient conf: Configuration) extends Serializable with LazyLogging{
+
+  def putAll(inputDf: DataFrame, rowKeyColumnName: String): Unit ={
+
+  }
 
 }
 object HBaseReadRepository {
---
+++ spark/src/main/scala/com/spike/spark_iceberg/repository/HBaseUtils.scala
@@ -0,0 +1,67 @@
+// scalastyle:off
+package com.spike.spark_iceberg.repository
+
+import com.typesafe.scalalogging.LazyLogging
+import org.apache.hadoop.hbase.client.{Delete, Put}
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.types.{DataType, LongType, StringType}
+
+object HBaseUtils extends Serializable with LazyLogging{
+
+  private val columnFamily: String = "cf"
+
+  def populatePutObject(row: Row,
+                        putObject: Put,
+                        hbaseColumnName: String,
+                        colDataType: DataType): Unit ={
+
+    val columnFamilyBytes = columnFamily.getBytes()
+    val hbaseColumnNameBytes = hbaseColumnName.getBytes()
+
+    colDataType match {
+      case StringType =>
+        Option(row.getAs[String](hbaseColumnName)) match {
+          case Some(r) =>
+            putObject.addColumn(columnFamilyBytes,hbaseColumnNameBytes, Bytes.toBytes(r))
+          case _ =>
+        }
+      case LongType =>
+        Option(row.getAs[Long](hbaseColumnName)) match {
+          case Some(r) =>
+            putObject.addColumn(columnFamilyBytes,hbaseColumnNameBytes, Bytes.toBytes(r))
+          case _ =>
+        }
+    }
+
+  }
+
+  def populatePutAndDeleteObject(row: Row,
+                                 putObject: Put,
+                                 deleteObject: Delete,
+                                 hbaseColumnName: String,
+                                 colDataType: DataType): Unit ={
+
+    val columnFamilyBytes = columnFamily.getBytes()
+    val hbaseColumnNameBytes = hbaseColumnName.getBytes()
+
+    colDataType match {
+      case StringType =>
+        Option(row.getAs[String](hbaseColumnName)) match {
+          case Some(r) =>
+            putObject.addColumn(columnFamilyBytes,hbaseColumnNameBytes, Bytes.toBytes(r))
+          case _ =>
+            deleteObject.addColumn(columnFamilyBytes,hbaseColumnNameBytes)
+        }
+      case LongType =>
+        Option(row.getAs[Long](hbaseColumnName)) match {
+          case Some(r) =>
+            putObject.addColumn(columnFamilyBytes,hbaseColumnNameBytes, Bytes.toBytes(r))
+          case _ =>
+            deleteObject.addColumn(columnFamilyBytes,hbaseColumnNameBytes)
+        }
+    }
+
+  }
+
+}
